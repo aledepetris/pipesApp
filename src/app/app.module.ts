30327d31f175dff4044893701679523358956d03
Alejandro Depetris <aledepetris@outlook.com>
2023-04-01 13:14:04 -0300

    Cambio en de idioma en la configuracion Locale.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
 import { VentasModule } from './ventas/ventas.module';
+
+// Cambiar el locale de la app.-
+import localeEs from "@angular/common/locales/es-AR";
+import { registerLocaleData } from "@angular/common";
+registerLocaleData( localeEs );
 
 @NgModule({
   declarations: [
@@ -17,7 +22,9 @@ import { VentasModule } from './ventas/ventas.module';
     VentasModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-AR' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
