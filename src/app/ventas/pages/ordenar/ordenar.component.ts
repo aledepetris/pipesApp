a9e9af011e54b6cc43a21f7460c4a92b653b7067
Alejandro Depetris <aledepetris@outlook.com>
2023-04-02 14:10:12 -0300

    Finalización de la App Pipes - Creación de pipes personhalizados. - Utilizacion de Table y Toolbar de ngPrime - Fin del contenido de la App.

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -10,8 +11,51 @@ export class OrdenarComponent {
 
   enMayusculas: boolean = true;
 
+  ordenarPor: string = '';
+
+  getHeroeColor( value: number): string {
+    return Color[value];
+  }
+
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Flash',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    },
+    {
+      nombre: 'Reverse Flash',
+      vuela: false,
+      color: Color.amarillo
+    }
+  ]
+
   cambiar() {
     this.enMayusculas = !this.enMayusculas;
   }
+
+  ordenar( valor: string ) {
+
+    this.ordenarPor = valor;
+
+  }
+
+
+
 
 }
